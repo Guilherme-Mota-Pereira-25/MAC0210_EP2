af39638efcc925ac3f2507c05ac193a225fa6698
Bozzetto <leobozzetto17@gmail.com>
2022-05-29 23:36:20 +0000

    Otimizei funcao de compressao e ajustei para o formato adequado

--- compress.m
+++ compress.m
@@ -1,11 +1,9 @@
 function compress(originalImg, k)
-   indexes = [];
-   i = 1;
-   for index = [1:size(originalImg)(1)]
-      if mod(index, k+1) == 0
-        indexes(i) = index;
-        i = i + 1;
-      endif
-   endfor
-   imwrite(originalImg(indexes,indexes,:),"compressed.bmp");
+  indexes = [];
+  i = 1;
+  for index = [1:k:size(originalImg)(1)]
+    indexes(i) = index;
+    i = i + 1;
+  endfor
+  imwrite(originalImg(indexes,indexes,:),"compressed.png",'Compression',"none",'Quality',0);
 endfunction
