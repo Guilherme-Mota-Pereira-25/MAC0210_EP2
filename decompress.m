4eb64832a78c485df13bb5c49c3c4dd3f8b2dcfc
Bozzetto <leobozzetto17@gmail.com>
2022-06-05 13:43:31 +0000

    Ajuste na decompress

--- decompress.m
+++ decompress.m
@@ -20,17 +20,17 @@ function decompress(compressedImg, method, k, h)
       endfor
     endfor
 				% Interpolação para a descompressão:
-  incr = h/(k+1);
-  decompressedImg = zeros(size(compressedImg)(1)*(1+k)-k,size(compressedImg)(2)*(1+k)-k,3);
-  for c = [1:3]
-    for i = [1:size(coefficients)(1)]
-      for j = [1:size(coefficients)(2)]
-        for x_inc = [0:k]
-          for y_inc = [0:k]
-            pos_desc_i = i*(k+1)+x_inc;
-            pos_desc_j = j*(k+1)+y_inc;
-            cf = coefficients(i,j,c,:);
-            decompressedImg(pos_desc_i,pos_desc_j,c) = cf(1) + cf(2)*(x_inc) +cf(3)*(y_inc) + cf(4)*(x_inc)*(y_inc);
+    incr = h/(k+1);
+    decompressedImg = zeros(size(compressedImg)(1)*(1+k)-k,size(compressedImg)(2)*(1+k)-k,3);
+    for c = [1:3]
+      for i = [1:size(coefficients)(1)]
+	for j = [1:size(coefficients)(2)]
+          for x_inc = [0:k]
+            for y_inc = [0:k]
+              pos_desc_i = i*(k+1)+x_inc;
+              pos_desc_j = j*(k+1)+y_inc;
+              cf = coefficients(i,j,c,:);
+              decompressedImg(pos_desc_i,pos_desc_j,c) = cf(1) + cf(2)*(x_inc) +cf(3)*(y_inc) + cf(4)*(x_inc)*(y_inc);
           endfor
         endfor
       endfor
@@ -51,45 +51,45 @@ function decompress(compressedImg, method, k, h)
 
   elseif method == 2
     size_c = size(compressedImg)(1);
-    coefficients = zeros(size_c-1,size_c-1,3,4,4);
+    coefficients = double(zeros(size_c-1,size_c-1,3,4,4));
     h_matrix = [1 0 0 0; 1 h h^2 h^3; 0 1 0 0; 0 1 2*h 3*h^2];
-    der = zeros(3,size_c,size_c,3);
+    der = double(zeros(3,size_c,size_c,3));
     for c = [1:3]
       for i = [1:size_c]
 	for j = [1:size_c]
-	  if(i = 1)
-	    der(1,1,j,c) = (compressedImg(2,j,c)-compressedImg(1,j,c))/h;
-	  elseif(i = size_c)
-	    der(1,size_c,j,c) = (compressedImg(size_c,j,c)-compressedImg(size_c-1,j,c))/h;
+	  if(i == 1)
+	    der(1,1,j,c) = double(compressedImg(2,j,c)-compressedImg(1,j,c))/(1.0*h);
+	  elseif(i == size_c)
+	    der(1,size_c,j,c) = double(compressedImg(size_c,j,c)-compressedImg(size_c-1,j,c))/(1.0*h);
 	  else
-	    der(1,i,j,c) = (compressedImg(i+1,j,c)-compressedImg(i-1,j,c))/(2*h);
+	    der(1,i,j,c) = double(compressedImg(i+1,j,c)-compressedImg(i-1,j,c))/(2.0*h);
 	  endif
 	  
-	  if(j = 1)
-	    der(2,i,1,c) = (compressedImg(i,2,c)-compressedImg(i,1,c))/h;
-	  elseif(j = size_c)
-	    der(2,i,size_c,c) = (compressedImg(i,size_c,c)-compressedImg(i,size_c-1,c))/h;
+	  if(j == 1)
+	    der(2,i,1,c) = double(compressedImg(i,2,c)-compressedImg(i,1,c))/(1.0*h);
+	  elseif(j == size_c)
+	    der(2,i,size_c,c) = double(compressedImg(i,size_c,c)-compressedImg(i,size_c-1,c))/(1.0*h);
 	  else
-	    der(2,i,j,c) = (compressedImg(i,j+1,c)-compressedImg(i,j-1,c))/(2*h);
+	    der(2,i,j,c) = double(compressedImg(i,j+1,c)-compressedImg(i,j-1,c))/(2.0*h);
 	  endif
 	endfor
       endfor
       
       for i = [1:size_c]
 	for j = [1:size_c]
-	  if(i = 1)
-	    der(3,1,j,c) = (der(2,2,j,c)-der(2,1,j,c))/h;
-	  elseif(i = size_c)
-	    der(3,size_c,j,c) = (der(2,size_c,j,c)-der(2,size_c-1,j,c))/h;
+	  if(i == 1)
+	    der(3,1,j,c) = double(der(2,2,j,c)-der(2,1,j,c))/(1.0*h);
+	  elseif(i == size_c)
+	    der(3,size_c,j,c) = double(der(2,size_c,j,c)-der(2,size_c-1,j,c))/(1.0*h);
 	  else
-	    der(3,i,j,c) = (der(2,i+1,j,c)-der(2,i-1,j,c))/(2*h);
+	    der(3,i,j,c) = double(der(2,i+1,j,c)-der(2,i-1,j,c))/(2.0*h);
 	  endif
 	endfor
       endfor
 
       for i = [1:size_c-1]
 	for j = [1:size_c-1]
-	  aux_M = zeros(4,4);
+	  aux_M = double(zeros(4,4));
 	  for x = [1:2]
 	    for y = [1:2]
 	      aux_M(x,y) = compressedImg(i+x-1,j+y-1,c);
@@ -115,10 +115,12 @@ function decompress(compressedImg, method, k, h)
 	for j = [1:size_c-1]
           for x_inc = [0:k]
             for y_inc = [0:k]
+	      x = x_inc*incr;
+	      y = y_inc*incr;
 	      pos_desc_i = i*(k+1)+x_inc;
 	      pos_desc_j = j*(k+1)+y_inc;
 	      cf = coefficients(i,j,c,:,:);
-	      decompressedImg(pos_desc_i,pos_desc_j,c) = [1 x_inc x_inc^2 x_inc^3] * reshape(cf,4,4) * [1; y_inc; y_inc^2; y_inc^3];
+	      decompressedImg(pos_desc_i,pos_desc_j,c) = [1 x x^2 x^3] * reshape(cf,4,4) * [1; y; y^2; y^3];
             endfor
           endfor
 	endfor
@@ -127,15 +129,16 @@ function decompress(compressedImg, method, k, h)
 	cf1 = coefficients(size_c - 1,i,c,:,:);
 	cf2 = coefficients(i,size_c - 1,c,:,:);
 	for g_inc = [0:k]
+	  g = g_inc * incr;
           pos_desc_i = i*(k+1)+g_inc;
-          decompressedImg(size(decompressedImg)(1),pos_desc_i,c) = [1 (k+1) (k+1)^2 (k+1)^3] * reshape(cf1,4,4) * [1; g_inc; g_inc^2; g_inc^3];
-          decompressedImg(pos_desc_i,size(decompressedImg)(2),c) = [1 g_inc g_inc^2 g_inc^3] * reshape(cf2,4,4) * [1; (k+1); (k+1)^2; (k+1)^3];
+          decompressedImg(size(decompressedImg)(1),pos_desc_i,c) = [1 h h^2 h^3] * reshape(cf1,4,4) * [1; g; g^2; g^3];
+          decompressedImg(pos_desc_i,size(decompressedImg)(2),c) = [1 g g^2 g^3] * reshape(cf2,4,4) * [1; h; h^2; h^3];
 	endfor
       endfor
       cff = coefficients(size_c-1,size_c-1,c,:,:);
-      decompressedImg(size(decompressedImg(1)),size(decompressedImg)(2),c) = [1 (k+1) (k+1)^2 (k+1)^3] * reshape(cff,4,4) * [1; (k+1); (k+1)^2; (k+1)^3];
+      decompressedImg(size(decompressedImg(1)),size(decompressedImg)(2),c) = [1 h h^2 h^3] * reshape(cff,4,4) * [1; h; h^2; h^3];
     endfor
   endif
   clear coefficients;
-  imwrite(uint8(decompressedImg),"decompressedbi.png",'Compression',"none",'Quality',0);
+  imwrite(uint8(decompressedImg),"decompressed.png",'Compression',"none",'Quality',0);
 endfunction
--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clear all;
 original = imread("input.png");
 compress(original,1);
 compressed = imread("compressed.png");
-decompress(compressed,1,1,1);
+decompress(compressed,2,1,2);
 decompressed = imread("decompressed.png");
 calculateError(original([1:255],[1:255],:),decompressed);
 imshow(original)
