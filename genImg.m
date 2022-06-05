6819e87594a1584ce8b6d070bbd09ec41f56e93a
Bozzetto <leobozzetto17@gmail.com>
2022-06-05 21:37:21 +0000

    Funcao geradora

--- genImg.m
+++ genImg.m
@@ -1,14 +1,12 @@
 function image = genImg()
-  x = 0; y = 0; h = pi; p = 128; % Variáveis de definição da imagem e de sua projeção em R²
+  x = 0; y = 0; h = pi; p = 601; % Variáveis de definição da imagem e de sua projeção em R²
   image = uint8(zeros(p,p,3));
   for i = [1:p]
     for j = [1:p]
-      image(i,j,1) = floor(255*(sin(rad2deg(x))+1)/2);
-      image(i,j,2) = floor(255*((sin(rad2deg(x))+sin(rad2deg(y)))/2+1)/2);
-      image(i,j,3) = image(i,j,1);
-      y = y + h;
+      image(i,j,1) = floor(255*(sind(i)+1)/2);
+      image(i,j,2) = floor(255*((sind(i)+sind(j))/2+1)/2);
+      image(i,j,3) = floor(255*(sind(j)+1)/2);
     endfor
-    x = x + h; 
   endfor
   imwrite(image,"genned.png",'Compression',"none",'Quality',0);
 endfunction
